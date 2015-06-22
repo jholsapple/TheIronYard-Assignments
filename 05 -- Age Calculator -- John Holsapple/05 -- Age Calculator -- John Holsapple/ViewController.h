f7cc6c62b63c83fed2b6145c3016145b964187e9
jholsapple <japple1000@yahoo.com>
2015-06-22 14:53:46 -0400

    Initial Commit
    started new homework

---
+++ 05 -- Age Calculator -- John Holsapple/05 -- Age Calculator -- John Holsapple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  05 -- Age Calculator -- John Holsapple
+//
+//  Created by John Holsapple on 6/22/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
