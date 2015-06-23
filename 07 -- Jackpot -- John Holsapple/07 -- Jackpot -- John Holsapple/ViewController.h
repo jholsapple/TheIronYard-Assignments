704f7a294e5155cb5cb76a83f010ecd1cfb64b8a
jholsapple <japple1000@yahoo.com>
2015-06-23 14:43:57 -0400

    New app
    new app called Jackpot

---
+++ 07 -- Jackpot -- John Holsapple/07 -- Jackpot -- John Holsapple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  07 -- Jackpot -- John Holsapple
+//
+//  Created by John Holsapple on 6/23/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 07 -- Jackpot -- John Holsapple/07 -- Jackpot -- John HolsappleTests/_7____Jackpot____John_HolsappleTests.m
@@ -0,0 +1,40 @@
+//
+//  _7____Jackpot____John_HolsappleTests.m
+//  07 -- Jackpot -- John HolsappleTests
+//
+//  Created by John Holsapple on 6/23/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _7____Jackpot____John_HolsappleTests : XCTestCase
+
+@end
+
+@implementation _7____Jackpot____John_HolsappleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
