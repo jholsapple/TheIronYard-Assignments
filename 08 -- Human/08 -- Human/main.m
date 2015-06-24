6fc1f0c3f654d642ae241735d3e62811342fef87
jholsapple <japple1000@yahoo.com>
2015-06-24 15:21:49 -0400

    Initial commit
    New homework

---
+++ 08 -- Human/08 -- Human/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  08 -- Human
+//
+//  Created by John Holsapple on 6/24/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
