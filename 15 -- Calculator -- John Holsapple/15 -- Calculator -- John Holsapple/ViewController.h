8f3f59cee8f4175178c398ae6f7b3fcb726e6507
jholsapple <japple1000@yahoo.com>
2015-07-03 14:49:50 -0400

    15 -- Calculator
    New assignment

---
+++ 15 -- Calculator -- John Holsapple/15 -- Calculator -- John Holsapple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  15 -- Calculator -- John Holsapple
+//
+//  Created by John Holsapple on 7/3/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
