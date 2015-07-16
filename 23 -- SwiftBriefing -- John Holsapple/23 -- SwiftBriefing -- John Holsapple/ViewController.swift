dc382eb30c5a8a47e272026e37529b544671bd9b
jholsapple <japple1000@yahoo.com>
2015-07-16 09:07:59 -0400

    Updates
    Mostly done

--- 23 -- SwiftBriefing -- John Holsapple/23 -- SwiftBriefing -- John Holsapple/ViewController.swift
+++ 23 -- SwiftBriefing -- John Holsapple/23 -- SwiftBriefing -- John Holsapple/ViewController.swift
@@ -19,26 +19,33 @@ class ViewController: UIViewController {
     {
         hideKeyboard()
         
-        if !agentNameTextField .isEqual(String) "" && !agentPasswordTextField.isEqual(String) ""
+        if !agentNameTextField.text.isEmpty && !agentPasswordTextField.text.isEmpty && agentNameTextField.text.componentsSeparatedByString(" ").count == 2
         {
-            var agentName = agentNameTextField.text
-            let nameComponents = [agentName .componentsSeparatedByString(" ")]
-            greetingLabel.text = String(format: "Good Evening, Agent", <#arguments: CVarArgType#>...)
+            let agentName = agentNameTextField.text
+            let nameComponents = agentName.componentsSeparatedByString(" ")
+            greetingLabel.text = "Good Evening, Agent \(nameComponents[1])"
+            
+            missionBriefing.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(nameComponents[1]), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
+            
+            self.view.backgroundColor = UIColor.greenColor()
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.redColor()
         }
     }
     
     @IBAction func viewTapped(sender: UITapGestureRecognizer)
     {
-        resignFirstResponder()
+        hideKeyboard()
     }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        agentNameTextField.text = ""
-        agentPasswordTextField.text = ""
-        greetingLabel.text = ""
-        missionBriefing.text = ""
+        self.agentNameTextField.text = ""
+        self.agentPasswordTextField.text = ""
+        self.missionBriefing.text = ""
     }
 
     override func didReceiveMemoryWarning()
@@ -49,7 +56,11 @@ class ViewController: UIViewController {
 
     func hideKeyboard()
     {
-        resignFirstResponder()
+        if agentPasswordTextField.isFirstResponder() || agentNameTextField.isFirstResponder()
+        {
+            agentPasswordTextField.resignFirstResponder()
+            agentNameTextField.resignFirstResponder()
+        }
     }
 
 }
