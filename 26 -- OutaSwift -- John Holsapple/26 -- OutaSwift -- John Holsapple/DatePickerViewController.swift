e0a3fb6907bcf9020558a13d931764efc35f72b0
jholsapple <japple1000@yahoo.com>
2015-07-20 13:55:27 -0400

    Changes to storyboard
    Now the storyboard is completed

---
+++ 26 -- OutaSwift -- John Holsapple/26 -- OutaSwift -- John Holsapple/DatePickerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DatePickerViewController.swift
+//  26 -- OutaSwift -- John Holsapple
+//
+//  Created by John Holsapple on 7/20/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController
+{
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
