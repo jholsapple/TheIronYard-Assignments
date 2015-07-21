982de59bc8af6c6474e360e4d7c53fe1751e730d
jholsapple <japple1000@yahoo.com>
2015-07-21 15:21:03 -0400

    New assignment created
    new homework. SwiftTeams created

---
+++ 27 -- SwiftTeams -- John Holsapple/27 -- SwiftTeams -- John Holsapple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  27 -- SwiftTeams -- John Holsapple
+//
+//  Created by John Holsapple on 7/21/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
