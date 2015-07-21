8b548d551ae91082b030b90c527b4668c5711fe8
jholsapple <japple1000@yahoo.com>
2015-07-21 19:27:41 -0400

    Updates

--- 13 -- SportsTeams/13 -- SportsTeams/TeamsTableViewController.m
+++ 13 -- SportsTeams/13 -- SportsTeams/TeamsTableViewController.m
@@ -39,17 +39,6 @@
         [teamStorage addObject: aTeam];
     }
     
-/*  Team *aTeam = [[Team alloc] init];
-    aTeam.teamName = @"Orlando Magic";
-    aTeam.league = @"NBA";
-    aTeam.starPlayer = @"Victor Oladipo";
-    
-    Team *anotherTeam = [[Team alloc] init];
-    anotherTeam.teamName = @"Detroit Lions";
-    anotherTeam.league = @"NFL";
-    anotherTeam.starPlayer = @"Matthew Stafford";
-*/
- 
     theTeams = [teamStorage copy];
     self.title = @"Sports Teams";
 }
@@ -90,49 +79,5 @@
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- 27 -- SwiftTeams -- John Holsapple/27 -- SwiftTeams -- John Holsapple/TeamsTableViewController.swift
+++ 27 -- SwiftTeams -- John Holsapple/27 -- SwiftTeams -- John Holsapple/TeamsTableViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class TeamsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
+class TeamsTableViewController: UITableViewController
+{
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        let theTeams = [["teamName": "Orlando Magic", "league": "NBA", "starPlayer": "Victor Oladipo", "wins": 25, "losses": 57], ["teamName": "Detroit Lions", "league": "NFL", "starPlayer": "Matthew Stafford", "wins": 11, "losses": 5], ["teamName": "Seattle Seahawks", "league": "NFL", "starPlayer": "Russell Wilson", "wins": 12, "losses": 4], ["teamName": "L.A. Lakers", "league": "NBA", "starPlayer": "Kobe Bryant", "wins": 21, "losses": 61], ["teamName": "Washington Nationals", "league": "MLB", "starPlayer": "Bryce Harper", "wins": 43, "losses": 34], ["teamName": "New England Patriots", "league": "NFL", "starPlayer": "Tom Brady", "wins": 12, "losses": 4], ["teamName": "New York Yankees", "league": "MLB", "starPlayer": "Babe Ruth", "wins": 41, "losses": 37], ["teamName": "Miami Heat", "league": "NBA", "starPlayer": "Dwayne Wade", "wins": 37, "losses": 45], ["teamName": "Philadelphia Eagles", "league": "NFL", "starPlayer": "Tim Tebow", "wins": 10, "losses": 6], ["teamName": "Cleveland Cavaliers", "league": "NBA", "starPlayer": "Kevin Love", "wins": 53, "losses": 29], ["teamName": "", "league": "PGA", "starPlayer":"Tiger Woods", "wins": 79, "losses": ""], ["teamName": "", "league": "Tennis", "starPlayer": "Serena Williams", "wins": 716, "losses": 121], ["teamName": "L.A. Galaxy", "league": "MLS", "starPlayer": "David Beckham", "wins": 7, "losses": 7]]
+        
+         var teamStorage = []()
+        
+        for var more in moreData
+        {
+            var Team aTeam = []()
+            teamName =
+            
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -20,20 +35,23 @@ class TeamsTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 0
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         return 0
