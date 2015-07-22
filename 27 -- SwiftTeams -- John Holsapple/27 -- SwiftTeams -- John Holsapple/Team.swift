93c1f863a991e775c69e2602ec2af0d11899090f
jholsapple <japple1000@yahoo.com>
2015-07-21 21:17:30 -0400

    Cell created
    Custom cell and tableView created in storyboard

---
+++ 27 -- SwiftTeams -- John Holsapple/27 -- SwiftTeams -- John Holsapple/Team.swift
@@ -0,0 +1,9 @@
+//
+//  Team.swift
+//  27 -- SwiftTeams -- John Holsapple
+//
+//  Created by John Holsapple on 7/21/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+import Foundation
---
+++ 27 -- SwiftTeams -- John Holsapple/27 -- SwiftTeams -- John Holsapple/TeamsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamsTableViewCell.swift
+//  27 -- SwiftTeams -- John Holsapple
+//
+//  Created by John Holsapple on 7/21/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class TeamsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
