d18fcf727586de6dc151c7ef988e477ce7fbe29d
jholsapple <japple1000@yahoo.com>
2015-07-28 21:42:25 -0400

    New assignment created
    32 — CoreList created

---
+++ 32 -- CoreList -- John Holsapple/32 -- CoreList -- John Holsapple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  32 -- CoreList -- John Holsapple
+//
+//  Created by John Holsapple on 7/28/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
