9bee644d0e1b6aa95c2ca8ceaece19bd337268d4
jholsapple <japple1000@yahoo.com>
2015-08-01 11:29:10 -0400

    created a new one
    new one is objective c

---
+++ 34 -- VenueMenu -- John Holsapple/34 -- VenueMenu -- John Holsapple/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  34 -- VenueMenu -- John Holsapple
+//
+//  Created by John Holsapple on 8/1/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ 34 -- VenueMenu -- John Holsapple/34 -- VenueMenu -- John Holsapple/SearchViewController.m
@@ -0,0 +1,42 @@
+//
+//  SearchViewController.m
+//  34 -- VenueMenu -- John Holsapple
+//
+//  Created by John Holsapple on 8/1/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
