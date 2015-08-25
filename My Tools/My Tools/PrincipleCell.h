b3067805a98496dc3684be67a4efbf72979a9046
jholsapple <japple1000@yahoo.com>
2015-08-25 18:48:47 -0400

    Pump ops done
    pump ops reference guide done

---
+++ My Tools/My Tools/PrincipleCell.h
@@ -0,0 +1,16 @@
+//
+//  PrincipleCell.h
+//  My Tools
+//
+//  Created by John Holsapple on 8/25/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrincipleCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UITextView *principleTextView;
+
+
+@end
